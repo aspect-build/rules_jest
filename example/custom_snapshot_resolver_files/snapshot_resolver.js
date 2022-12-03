91d3b64f502fae561d2743aab3c1f023d4545685
Greg Magolan <gmagolan@gmail.com>
2022-12-02 18:25:11 -0800

    feat: add support for updating __snapshots__ directories from output directory artifacts

--- example/custom_snapshot_resolver_files/snapshot_resolver.js
+++ example/custom_snapshot_resolver_files/snapshot_resolver.js
@@ -4,14 +4,11 @@ const DOT_EXTENSION = `.snap`;
 
 module.exports = {
   resolveSnapshotPath: (testPath) =>
-    path.join(
-      path.join(path.dirname(testPath), "__my_snapshots__"),
-      path.basename(testPath) + DOT_EXTENSION
-    ),
+    path.join(path.dirname(testPath), path.basename(testPath) + DOT_EXTENSION),
 
   resolveTestPath: (snapshotPath) =>
     path.join(
-      path.dirname(path.dirname(snapshotPath)),
+      path.dirname(snapshotPath),
       path.basename(snapshotPath, DOT_EXTENSION)
     ),
 
--- jest/private/bazel_snapshot_reporter.js
+++ jest/private/bazel_snapshot_reporter.js
@@ -1,14 +1,15 @@
 class BazelSnapshotReporter {
   onRunComplete(_, results) {
     if (results.numFailedTests && results.snapshot.failure) {
-      console.log(`================================================================================
+      console.log(`
+================================================================================
       
-      Snapshot failed, you can update the snapshot by running
-      bazel run ${process.env["TEST_TARGET"].replace(
-        /_bin$/,
-        ""
-      )}_update_snapshots
-      `);
+Update snapshots by running,
+
+  bazel run ${process.env["TEST_TARGET"].replace(/_bin$/, "")}_update_snapshots
+
+================================================================================
+`);
     }
   }
 }
