8af3e0cdc0812dcc010aa9e9f1915ff5f54e310a
Greg Magolan <gmagolan@gmail.com>
2023-02-16 23:10:38 -0800

    chore: update snapshots hint

--- jest/private/bazel_snapshot_reporter.js
+++ jest/private/bazel_snapshot_reporter.js
@@ -8,6 +8,11 @@ Update snapshots by running,
 
   bazel run ${process.env["TEST_TARGET"].replace(/_bin$/, "")}_update_snapshots
 
+New '__snapshots__' directories must be created manually before running
+'${process.env["TEST_TARGET"].replace(/_bin$/, "")}_update_snapshots' for them
+to be included in the update target if 'snapshots' is set to 'True' or a custom
+glob pattern.
+
 ================================================================================
 `);
     }
