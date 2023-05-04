a46ef9068530094af2ab766c0f8968462f0ba570
thesayyn <thesayyn@gmail.com>
2023-05-04 16:21:04 -0700

    update e2e

--- e2e/case12.sh
+++ e2e/case12.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-# Case 7: generate a coverage report with split coverage postprocessing
+# Case 12: generate a coverage report with split coverage postprocessing
 bazel coverage //jest/tests:case12 --instrument_test_targets --experimental_split_coverage_postprocessing --experimental_fetch_all_coverage_outputs
 
 COVERAGE_FILE="bazel-testlogs/jest/tests/case12/coverage.dat"
