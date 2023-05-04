3ef0d3ae0325ee749ee6c72f81635d1d68a39122
thesayyn <thesayyn@gmail.com>
2023-05-04 16:48:54 -0700

    fix shebang

--- jest/private/merger.sh
+++ jest/private/merger.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 
 # When --experimental_split_coverage_postprocessing is enabled copy the
 # $COVERAGE_DIR/coverage.dat to final destination that is $COVERAGE_OUTPUT_FILE
-if [ $SPLIT_COVERAGE_POST_PROCESSING == 1 ]; then
+if [[ $SPLIT_COVERAGE_POST_PROCESSING == 1 ]]; then
     cp "${COVERAGE_DIR}/coverage.dat" "${COVERAGE_OUTPUT_FILE}"
 fi
 
