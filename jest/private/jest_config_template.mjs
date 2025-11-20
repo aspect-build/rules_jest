072d10afce9fed3a4b42c1d71f5347f9630273ff
deacy <chrisdothtml@gmail.com>
2025-11-20 21:57:54 +0000

    cleanify

--- jest/private/jest_config_template.mjs
+++ jest/private/jest_config_template.mjs
@@ -55,18 +55,13 @@ function _hasReporter(config, name) {
   if (!config.reporters) {
     config.reporters = [];
   }
-  for (const r of config.reporters) {
+  return config.reporters.some((r) => {
     if (Array.isArray(r)) {
-      if (r.length > 0 && r[0] == name) {
-        return true;
-      }
+      return r.length > 0 && r[0] == name;
     } else {
-      if (r == name) {
-        return true;
-      }
+      return r == name;
     }
-  }
-  return false;
+  });
 }
 
 function _addReporter(config, name, reporter = undefined) {
