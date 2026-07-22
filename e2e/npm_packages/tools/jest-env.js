937e3bc617e58ba3533c4189ae2ffa1739603b96
Jason Bedard <jason+github@jbedard.ca>
2026-07-22 01:22:46 -0700

    test: reduce unnecessary use of @babel/preset-env to reduce problematic babel plugins

--- e2e/npm_packages/tools/jest-env.js
+++ e2e/npm_packages/tools/jest-env.js
@@ -1,10 +1,12 @@
 // an import of a third-party package
-const { jest } = require('@jest/globals');
+// Note: the binding is not named `jest` to avoid colliding with the `jest`
+// local that jest-runtime injects into every module scope.
+const jestGlobals = require("@jest/globals");
 
 // a reference to a linked workspace project
-const  { id: e2eLibId } = require('@e2e/lib')
+const { id: e2eLibId } = require("@e2e/lib");
 
-global.globalJestEnvInit = function() {
-    jest.useFakeTimers();
-    global.foo = e2eLibId()
-}
+global.globalJestEnvInit = function () {
+  jestGlobals.jest.useFakeTimers();
+  global.foo = e2eLibId();
+};
