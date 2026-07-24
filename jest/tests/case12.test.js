503ea0b60c9a48b0d6be82c700663ddcad60af42
Jason Bedard <jason+github@jbedard.ca>
2026-07-24 01:28:30 -0700

    ci: reorganize tasks

--- jest/tests/case12.test.js
+++
@@ -1,5 +0,0 @@
-const { foobar } = require("./case12.index.js");
-
-test("foobar", () => {
-  expect(foobar()).toEqual("foobar");
-});
--- jest/tests/case6.test.js
+++
@@ -1,5 +0,0 @@
-const { foobar } = require("./case6.index.js");
-
-test("foobar", () => {
-  expect(foobar()).toEqual("foobar");
-});
